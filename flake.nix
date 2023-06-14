0b6dc38159a7a5d870d06d2dfce7adbf0c71ebb0
chayleaf <chayleaf-git@pavluk.org>
2023-06-15 06:30:03 +0700

    add the actual lib to flake outputs

--- flake.nix
+++ flake.nix
@@ -1,9 +1,20 @@
 {
-  description = "A Nix DSL that compiles to Nftables";
+  description = "A Nix DSL that compiles to Nftables JSON";
 
-  outputs = { self, nixpkgs }: {
-    checks.x86_64-linux.default = let pkgs = nixpkgs.legacyPackages.x86_64-linux; in pkgs.callPackage ./checks.nix {
-      flake = (pkgs.callPackage ./. { }).config.notnft;
+  outputs = { self, nixpkgs }:
+    let
+      forEachSystem = func: nixpkgs.lib.genAttrs [ "aarch64-linux" "aarch64-darwin" "x86_64-darwin" "x86_64-linux" ] (system: {
+        inherit system;
+        result = func { inherit system; pkgs = import nixpkgs { inherit system; }; };
+      });
+    in
+    {
+      nixosModules.default = ./.;
+      # this isn't an officially supported third-party flake output
+      # homeManagerModules.default = ./.;
+      lib = forEachSystem ({ pkgs, ... }: (import ./. { inherit pkgs; inherit (pkgs) lib; }).config.notlua);
+      checks.x86_64-linux.default = let pkgs = nixpkgs.legacyPackages.x86_64-linux; in pkgs.callPackage ./checks.nix {
+        flake = (pkgs.callPackage ./. { }).config.notnft;
+      };
     };
-  };
 }
